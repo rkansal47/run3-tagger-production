aac12ab5f4f7d123b7deb17ab5d97fee23b256b1
Raghav Kansal <raghavk1997@yahoo.co.in>
2023-05-26 16:22:53 -0500

    scale m_res linearly

--- event_producer/cmsconnect_miniaods_UL17/fragments/BulkGravitonToHHTo4W_MX-800to8000_MH-260to650_JHUVariableWMass.py
+++ event_producer/cmsconnect_miniaods_UL17/fragments/BulkGravitonToHHTo4W_MX-800to8000_MH-260to650_JHUVariableWMass.py
@@ -20,11 +20,12 @@ import numpy as np
 
 # low mass list
 low_m_higgs = np.array([15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250])
+low_m_res = np.arange(600, 6000, 100)
 
 # high mass list
 m_higgs = np.arange(260, 660, 10)
-m_res = np.arange(600, 6000, 100)
-mpoints = [(mx, mh) for mx in m_res for mh in m_higgs]
+# minimum m_res changes s.t. it is always > 2x m_higgs to avoid off-shell Higgses
+m_res_min = np.linspace(600, 1600, len(m_higgs))  
 
 # reweight points such that there are the same number of events at 260 as 250 GeV
 # and then continuously decrease the weight from there till 650 GeV
@@ -43,21 +44,23 @@ def mh_weight(mh):
     return a + d * (len(m_higgs) - idx - 1)
 
 
-for mx, mh in mpoints:
-    # print('BulkGravitonToHH_MX%.0f_MH%.0f' % (mx, mh))
-    generator.RandomizedParameters.append(
-        cms.PSet(
-            ConfigWeight = cms.double(mh_weight(mh)),
-            GridpackPath =  cms.string('instMG://BulkGravitonToHH_MX-600to6000_MH-260to650/MG5_aMC_v2.6.5/%.0f:%.0f' % (mx, mh)),
-            ConfigDescription = cms.string('BulkGravitonToHH_MX%.0f_MH%.0f' % (mx, mh)),
-            PythiaParameters = cms.PSet(
-                pythia8CommonSettingsBlock,
-                pythia8CP5SettingsBlock,
-                pythia8PSweightsSettingsBlock,
-                parameterSets = cms.vstring('pythia8CommonSettings',
-                                            'pythia8CP5Settings',
-                                            'pythia8PSweightsSettings',
-		        )
+for i, mh in enumerate(mpoints):
+    m_res = np.linspace(m_res_min[i], m_res_min[i] * 10, len(low_m_res), endpoint=False)
+    for mx in m_res:
+        # print('BulkGravitonToHH_MX%.0f_MH%.0f' % (mx, mh))
+        generator.RandomizedParameters.append(
+            cms.PSet(
+                ConfigWeight = cms.double(mh_weight(mh)),
+                GridpackPath =  cms.string('instMG://BulkGravitonToHH_MX-600to6000_MH-260to650/MG5_aMC_v2.6.5/%.0f:%.0f' % (mx, mh)),
+                ConfigDescription = cms.string('BulkGravitonToHH_MX%.0f_MH%.0f' % (mx, mh)),
+                PythiaParameters = cms.PSet(
+                    pythia8CommonSettingsBlock,
+                    pythia8CP5SettingsBlock,
+                    pythia8PSweightsSettingsBlock,
+                    parameterSets = cms.vstring('pythia8CommonSettings',
+                                                'pythia8CP5Settings',
+                                                'pythia8PSweightsSettings',
+                    )
+                )
             )
         )
-    )
